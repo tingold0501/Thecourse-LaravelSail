22bb8f9483fed071ea8d5a0dffb2254c346a1ba1
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-01-28 14:42:40 +0700

    .....

---
+++ thecourse-website-laravelsail/database/migrations/2024_01_28_071655_create_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('summary');
+            $table->bigInteger('price');
+            $table->string('image');
+            $table->bigInteger('discount');
+            $table->bigInteger('duration');
+            $table->string('Grade');
+            $table->boolean('status')->default(1);
+            $table->text('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
