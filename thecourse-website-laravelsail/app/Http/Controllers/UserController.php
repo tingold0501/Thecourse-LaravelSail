2975ed876c70e33b30d0d3c37b59e7990ecf630a
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-02-01 20:26:05 +0700

    .....

--- thecourse-website-laravelsail/app/Http/Controllers/UserController.php
+++ thecourse-website-laravelsail/app/Http/Controllers/UserController.php
@@ -6,7 +6,6 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Mail;
 class UserController extends Controller
 {
     /**
