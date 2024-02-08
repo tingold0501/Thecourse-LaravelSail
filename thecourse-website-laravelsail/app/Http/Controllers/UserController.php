8cab3a38abf678ff62f22b1fc2f6f7f3d5b51ea1
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-02-08 09:57:21 +0700

    .....

--- thecourse-website-laravelsail/app/Http/Controllers/UserController.php
+++ thecourse-website-laravelsail/app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
     /**
@@ -46,7 +47,27 @@ class UserController extends Controller
      * Show the form for creating a new resource.
      */
     public function loginTeacher(Request $request, User $user){
-        
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'password' => 'required',
+                'email' => 'required|email|exists:users,email',
+            ],
+            [
+                'password.required' => 'Thiếu mật khẩu tài khoản',
+                'email.required' => 'Thiếu email tài khoản',
+                'email.email' => 'Email tài khoản không hợp lệ',
+                'email.exists' => 'Email tài khoản không tồn tại',
+            ],
+        );
+        if ($validator->fails()) {
+            return response()->json(['check' => false, 'msg' => $validator->errors()]);
+        }
+        if(Auth::attempt(['email' => $request->email, 'password' =>  $request->password,'status'=>1,'role_id'=>1],true)){
+            return response()->json(['check'=>true,'token'=>Auth::user()->remember_token]);
+        }else{
+            return response()->json(['check'=>false,'msg'=>'Tài khoàn đăng nhập không hợp lệ']);
+        }
     }
     public function create(Request $request, User $user)
     {
--- thecourse-website-laravelsail/routes/api.php
+++ thecourse-website-laravelsail/routes/api.php
@@ -29,6 +29,8 @@ Route::controller(UserController::class)->group(function () {
     Route::post('updateStatusUser', 'updateStatus');
     Route::post('updateEmailUser', 'updateEmail');
     Route::post('updatePhonelUser', 'updatePhone');
+    Route::post('loginTeacher', 'loginTeacher');
+
 });
 
 Route::controller(RoleController::class)->group(function () {
